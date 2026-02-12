ac316a432bf43cd82227a2c6a54961056c2d6d83
Lilia Ospino <lilupao2202@gmail.com>
2026-02-12 14:21:31 -0500

    Agregando ejercicio 6

---
+++ ejercicios/ej06.c
@@ -0,0 +1,26 @@
+/*
+Ejercicio 6
+Leer un número entero en la variable VALOR. Utilizar operadores de bits (bitwise)
+para determinar si el número es par o impar (verificando el bit menos significativo)
+y mostrar el resultado. No está permitido usar el operador de módulo (%).
+
+Integrantes:
+-Lilia Ospino, Moises Del Toro, Diego Luengas, Keyner Viloria
+
+Compilador usado:  GCC (OnlineGDB)
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int VALOR = 2202;  
+    int lsb = VALOR & 1;
+
+    if (lsb == 0) {
+        printf("{ 'VALOR' : %d, 'Resultado' : 'PAR' }\n", VALOR);
+    } else {
+        printf("{ 'VALOR' : %d, 'Resultado' : 'IMPAR' }\n", VALOR);
+    }
+
+    return 0;
+}
